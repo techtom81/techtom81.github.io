acf519e0a21388bc4593db5f0a960e7ab5c20176
Tom Hunt <tom.hunt@damdigital.com>
2019-12-08 18:58:32 +0000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# techtom81.github.io
-A React app for authenticated photo viewing, downloading and uploading
---
+++ precache-manifest.8c547241042d5c2792212277197e55ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca46f569126780d9ab3df44b2ca2617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7032e596edaeb3d0955",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "a323a2f2433192825a76",
+    "url": "/static/css/main.08211a4b.chunk.css"
+  },
+  {
+    "revision": "d7032e596edaeb3d0955",
+    "url": "/static/js/2.42d396eb.chunk.js"
+  },
+  {
+    "revision": "c940a7c2fcb5003859cf9f11c4902fce",
+    "url": "/static/js/2.42d396eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a323a2f2433192825a76",
+    "url": "/static/js/main.e0e96d8f.chunk.js"
+  },
+  {
+    "revision": "abfb2bf60cbe905a84d7",
+    "url": "/static/js/runtime-main.8825b82a.js"
+  },
+  {
+    "revision": "442fb73152e89df25fb5f2eeb9c08a9e",
+    "url": "/static/media/banner2-home.442fb731.jpg"
+  },
+  {
+    "revision": "0eea66e1c3ccc246ce5308bf4a46b9ff",
+    "url": "/static/media/login2.0eea66e1.jpg"
+  }
+]);
